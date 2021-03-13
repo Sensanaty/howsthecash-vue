d29445f6c7b5b8a592d686b6cdd4f8dfe3ef9ed0
Sensanaty <salevic@lu-ka.me>
2021-03-13 15:59:34 +0100

    Add example routes to the project

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div id="root">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 
 <script>
