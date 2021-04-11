0ae10c84e3e1900dc16ec9baa6684e349a903cdb
Sensanaty <salevic@lu-ka.me>
2021-04-11 14:10:43 +0200

    Adjust the vite config file
    - Globally import a SCSS file housing variables that will be used in the app
    - Configure Voie's import rules to make pages async

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ import voie from "vite-plugin-voie";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), voie()]
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@import \"./src/assets/styles/_variables.scss\";"
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    voie({
+      importMode(path) {
+        return path.includes("index") ? "sync" : "async";
+      }
+    })
+  ]
 });
