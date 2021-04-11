16a7bb6d97c5010d4462947f5061073173ab7377
Sensanaty <salevic@lu-ka.me>
2021-04-11 14:18:34 +0200

    Implement basic auth flow and introduce basic styling to the pages
    App.vue:
    - Added a Sidebar component
    - Implemented transitions on the Router View
    
    main.scss:
    - Made ParseGrotesk available to use everywhere in the app
    - Implemented some CSS stylings for the Login and Signup forms
    
    MainHeader.vue:
    - Inserted the How's The Cash logo to the header
    - Adjusted some links, included a link to the Dashboard
    - Implemented a Logout button and logout functionality via the store
    - Moved the login logic & form into the dedicated Login page
    - Adjusted the styling of the component
    
    Sidebar.vue:
    - Introduced a new Sidebar component that will show up when a user is on the Dashboard
    
    [...notFound].vue is a catch-all route
    
    Dashboard/index.vue:
    - Created the new dashboard page that is offset by 150 due to the sidebar
    
    Implemented new login and signup pages
    
    `store/modules/user`:
    - Added an error entry into the store
    - Implemented new getters, mutations and actions for the general login flow

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="root">
-    <MainHeader />
-    <RouterView />
+    <transition name="slide-down">
+      <MainHeader v-if="this.$route.name !== 'dashboard'" />
+    </transition>
+    <transition name="slide-in">
+      <Sidebar v-if="this.$route.name === 'dashboard'" />
+    </transition>
+    <RouterView v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </RouterView>
   </div>
 </template>
 
@@ -10,19 +19,25 @@
   import "./assets/styles/main.scss";
 
   import MainHeader from "./components/MainHeader.vue";
+  import Sidebar from "./components/Sidebar.vue";
 
   export default {
     name: "App",
     components: {
+      Sidebar,
       MainHeader
     },
-    beforeMount() {
-      if (this.$store.state.user.token) {
-        this.$store.dispatch("user/fetchUserInfo");
-      }
-    }
   };
 </script>
 
 <style lang="scss">
+  .fade-enter-active { transition: opacity 100ms ease }
+  .fade-leave-active { transition: opacity 50ms ease }
+  .fade-enter-from, .fade-leave-to { opacity: 0 }
+
+  .slide-in-enter-active, .slide-in-leave-active { transition: transform 150ms ease }
+  .slide-in-enter-from, .slide-in-leave-to { transform: translate(-100%, 0) }
+
+  .slide-down-enter-active, .slide-down-leave-active { transition: transform 100ms ease }
+  .slide-down-enter-from, .slide-down-leave-to { transform: translate(0, -100%) }
 </style>
---
+++ src/pages/signup/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div >
+    <form id="signup" class="form-wrapper">
+      <h1>Sign Up</h1>
+      <label for="username">Username</label>
+      <input id="username" type="text" v-model="username" placeholder="Required">
+      <label for="password">Password</label>
+      <input id="password" type="password" v-model="password" placeholder="Required">
+      <label for="repeat">Confirm Password</label>
+      <input id="repeat" type="password" v-model="confirmPassword" placeholder="Required">
+      <label for="email">Email</label>
+      <input id="email" type="email">
+      <button type="button" @click="handleSignup">Sign Up</button>
+      <span class="error-message"></span>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Logout",
+    data() {
+      return {
+        username: "",
+        password: "",
+        confirmPassword: "",
+        email: ""
+      };
+    },
+    methods: {
+      handleSignup() {
+        this.$store.dispatch("user/signup", { username: this.username, password: this.password, email: this.email });
+      },
+    }
+  };
+</script>
