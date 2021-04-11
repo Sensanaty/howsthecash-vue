da61d4ec3747ddb0cdd8ec2da4cfbb41f62681ce
Sensanaty <salevic@lu-ka.me>
2021-04-11 14:16:50 +0200

    Adjust the router
    - Deleted some whitespace
    - Made a beforeEach hook to check whether a user has to be authenticated to access a particular route
    - Made a afterEach hook to change the title of the page after navigation

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,23 @@ import { createRouter, createWebHistory } from "vue-router";
 import routes from "voie-pages";
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+  if (to.name === "dashboard" && !token) {
+    next("/login");
+  } else if ((to.name === "signup" || to.name === "login") && token) {
+    next("/dashboard");
+  } else next();
+});
+
+router.afterEach((to) => {
+  if (to.name === "index") {
+    document.title = "How's The Cash?";
+  } else {
+    document.title = String(to.name).charAt(0).toUpperCase() + String(to.name).slice(1);
+  }
 });
